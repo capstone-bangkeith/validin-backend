17208a810df18ab7d868006f9670bf71ef1dffcb
LordRonz <aarontanhar2000@gmail.com>
2022-05-22 14:03:32 +0700

    feat: dotenv config :rocket:

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,9 @@
-export const PORT = 8080;
+import { config } from 'dotenv';
+import { join as pathJoin } from 'path';
+
+config({ path: pathJoin(__dirname, '../../.env') });
+
+export const PORT = process.env.PORT ? +process.env.PORT : 3001;
 
 export default {
   PORT,
