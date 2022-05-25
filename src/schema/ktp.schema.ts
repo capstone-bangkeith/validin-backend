8c67ccd7dfab33d0399c40dffdf6a6456c1d7135
LordRonz <aarontanhar2000@gmail.com>
2022-05-25 19:17:45 +0700

    feat: add pm2 :rocket:

---
+++ src/schema/ktp.schema.ts
@@ -0,0 +1,61 @@
+import { Type } from '@sinclair/typebox';
+import type { FastifySchema } from 'fastify';
+
+import { ErrorResponse } from './error.schema';
+
+export const KtpType = Type.Object(
+  {
+    nama: Type.String(),
+    nik: Type.String(),
+    alamat: Type.String(),
+    rtRw: Type.String(),
+    kelDesa: Type.String(),
+    kecamatan: Type.String(),
+    agama: Type.String(),
+    statusPerkawinan: Type.String(),
+    pekerjaan: Type.String(),
+    kewarganegaraan: Type.String(),
+  },
+  { description: 'Kode wilayah properties' }
+);
+
+const PostKtpResponse = Type.Object({
+  data: KtpType,
+});
+
+const ktpSchemaBase: FastifySchema = {
+  description: 'Post KTP form',
+};
+
+export const ktpSchemaPost: FastifySchema = {
+  ...ktpSchemaBase,
+  body: KtpType,
+};
+
+export const ktpSchemaGet: FastifySchema = {
+  ...ktpSchemaBase,
+  querystring: Type.Optional(
+    Type.Object({
+      kode: Type.Optional(Type.String({ description: 'KTP' })),
+      limit: Type.Optional(Type.Number({ minimum: 1 })),
+      page: Type.Optional(Type.Number({ minimum: 1 })),
+    })
+  ),
+  response: {
+    200: PostKtpResponse,
+    404: ErrorResponse,
+  },
+};
+
+export const ktpSchemaGetUnique: FastifySchema = {
+  ...ktpSchemaBase,
+  params: Type.Object({
+    kode: Type.Optional(Type.String({ description: 'Kode wilayah' })),
+  }),
+  response: {
+    200: Type.Object({
+      data: KtpType,
+    }),
+    404: ErrorResponse,
+  },
+};
