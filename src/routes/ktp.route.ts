00eb651f263d01e7ca3e0503f5981952e1b9f42f
LordRonz <aarontanhar2000@gmail.com>
2022-06-14 22:29:17 +0700

    feat: test route saatnya menggatot :rocket:

--- src/routes/ktp.route.ts
+++ src/routes/ktp.route.ts
@@ -571,10 +571,41 @@ export const plugin: FastifyPluginAsync = async (fastify) => {
 
       return reply.send({
         data: {
-          ktp: { ...ktp, kota, provinsi },
+          ktp,
           user,
         },
       });
+    }
+  );
+
+  fastify.post<{ Body: IOCRBody; Headers: IHeaders }>(
+    '/ocr-test',
+    { schema: ktpOcrSchemaPost },
+    async (request, reply) => {
+      const { data, mimetype } = request.body.ktp[0];
+
+      const { left, top, right, bottom } = request.body;
+
+      const processedImg: Sharp =
+        left !== undefined &&
+        top !== undefined &&
+        right !== undefined &&
+        bottom !== undefined
+          ? sharp(data).extract({
+              left: Math.round(Math.min(+left, +right)),
+              top: Math.round(Math.min(+bottom, +top)),
+              width: Math.round(Math.abs(+right - +left)),
+              height: Math.round(Math.abs(+bottom - +top)),
+            })
+          : sharp(data);
+
+      const ktpImg = await processedImg.toBuffer();
+      const filename = `ktp/test.${mime.extension(mimetype)}`;
+      const file = bucket.file(filename);
+      await file.save(ktpImg);
+      const publicUrl = file.publicUrl();
+
+      return reply.send({ publicUrl });
     }
   );
 };
